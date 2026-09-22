5e6732d5a39d8efd59ea31597198068bc0dd8d2d
regina <reginaadiba@gmail.com>
2026-09-23 06:42:27 +0700

    update storage file

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -58,7 +58,7 @@ class ItemController extends Controller
             $file = $request->file('image_file');
             $fileName = $file->getClientOriginalName();
             $newName = Carbon::now()->timestamp.'_'.$fileName;
-            Storage::putFileAs('items', $file, $newName);
+            Storage::disk('public')->putFileAs('items', $file, $newName);
 
             $request['image'] = $newName;
         }
