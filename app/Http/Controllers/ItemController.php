7dce0e64288bc132bb8cbe265bf200f1105d9a3a
regina <reginaadiba@gmail.com>
2026-09-23 11:07:54 +0700

    public storage

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -58,6 +58,7 @@ class ItemController extends Controller
             $file = $request->file('image_file');
             $fileName = $file->getClientOriginalName();
             $newName = Carbon::now()->timestamp.'_'.$fileName;
+            
             Storage::disk('public')->putFileAs('items', $file, $newName);
 
             $request['image'] = $newName;
